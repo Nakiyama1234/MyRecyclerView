ee5b22b97f53bf0b39b2bd2cb7f7e2a15b8a0f26
Nakiyama123 <naufalidn2009@gmail.com>
2023-11-22 16:04:02 +0700

    MyRecyclerView - Data View

--- app/src/main/java/com/naufal/myrecyclerview/MainActivity.kt
+++ app/src/main/java/com/naufal/myrecyclerview/MainActivity.kt
@@ -2,10 +2,24 @@ package com.naufal.myrecyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var rvHero: RecyclerView
+    private val list = ArrayList<Hero>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        rvHero = findViewById(R.id.rv_heros)
+        rvHero.setHasFixedSize(true)
+
+        list.add(getListHeroes())
+        showRecyclerList()
+    }
+
+    private fun getListHeroes(): ArrayList<Hero>{
+        val dataName =
     }
 }
